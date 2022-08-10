a033f9333db6ce5c9cdeba0cb37c9efad34ebdbf
Warrios974 <kenny.lauret@outlook.fr>
2022-08-10 13:32:30 +0400

    Fonction pour fermer la modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const radioCheck = document.querySelectorAll(".checkbox-input");
+const modalCloseBtn = document.querySelector(".close");
+////// Input form
+const formfirtInput = document.getElementById("first");
+const formlastInput = document.getElementById("last");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalCloseBtn.addEventListener("click", closeModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
