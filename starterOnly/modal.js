e12f25938d9f65aad8157a365068ab25b64d2090
Warrios974 <kenny.lauret@outlook.fr>
2022-08-12 16:58:21 +0400

    Mise en place de la validation du formulaire

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,7 +7,8 @@ function editNav() {
   }
 }
 
-let tournament = false;
+let cityTournament = false;
+let termsOfUse = true;
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
@@ -15,18 +16,33 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const radioCheck = document.querySelectorAll(".checkbox-input");
 const modalCloseBtn = document.querySelector(".close");
-////// Input form
-const formfirtInput = document.getElementById("first");
-const formlastInput = document.getElementById("last");
+
+const radioCheckCity = document.querySelectorAll(".checkbox-city");
+const termsOfUseId = document.querySelector("#checkbox1");
+const validateMessage = document.querySelector(".validateMessage");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 modalCloseBtn.addEventListener("click", closeModal);
 
-radioCheck.forEach((imput) => imput.addEventListener("click", function(){
-  tournament = true;
-  console.log(tournament);
+radioCheckCity.forEach((input) => input.addEventListener("click", function(){
+  cityTournament = true;
 }));
+
+termsOfUseId.addEventListener("click", function(){
+  if(termsOfUse){
+    termsOfUse = false;
+  }else{
+    termsOfUse = true;
+  }
+});
+
+// launch validate function input
+formData[0].addEventListener("input", function(e){twoCaractereOrmore(e,0);});
+formData[1].addEventListener("input", function(e){twoCaractereOrmore(e,1);});
+formData[2].addEventListener("input", function(e){emailValidate(e);});
+formData[3].addEventListener("input", function(e){birthdayValidate(e);});
+formData[4].addEventListener("input", function(e){participationValidate(e);});
 
 // launch modal form
 function launchModal() {
@@ -38,8 +54,70 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
-function validate(e){
-  if(tournament == false){
+// Function validate value input ////////////////////////////////////////////////////////////////
+function twoCaractereOrmore(e,n){
+  let isValid = false;
+  var value = e.target.value;
+  if (value.length >= 2) {
+      isValid = true;
+      formData[n].removeAttribute("data-error");
+      formData[n].removeAttribute("data-error-visible");
+  } else {
+      isValid = false;
+      formData[n].setAttribute("data-error", "Veuillez entrer 2 caractères ou plus.");
+      formData[n].setAttribute("data-error-visible", "true");
+  }
+}
+
+function emailValidate(e){
+  var value = e.target.value;
+  if(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value)){
+    formData[2].removeAttribute("data-error");
+    formData[2].removeAttribute("data-error-visible");
+  }else{
+    formData[2].setAttribute("data-error", "Veuillez entrer un email valide s'il vous plaît.");
+    formData[2].setAttribute("data-error-visible", "true");
+  }
+}
+
+function birthdayValidate(e){
+  var value = e.target.value;
+  if(value){
+    formData[3].removeAttribute("data-error");
+    formData[3].removeAttribute("data-error-visible");
+  }else{
+    formData[3].setAttribute("data-error", "Vous devez entrer votre date de naissance.");
+    formData[3].setAttribute("data-error-visible", "true");
+  }
+}
+
+function participationValidate(e){
+  var value = e.target.value;
+  if(/^([0-9]|[1-9][0-9]|100)$/.test(value)){
+    formData[4].removeAttribute("data-error");
+    formData[4].removeAttribute("data-error-visible");
+  }else{
+    formData[4].setAttribute("data-error", "Veuillez entrer un chiffre s'il vous plaît.");
+    formData[4].setAttribute("data-error-visible", "true");
+  }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Function valider
+function validate(){
+  if(cityTournament == false){
+    formData[5].setAttribute("data-error", "Vous devez choisir une option.");
+    formData[5].setAttribute("data-error-visible", "true");
     return false;
+  }else{
+    if(termsOfUse == false){
+      formData[6].setAttribute("data-error", "Vous devez vérifier que vous acceptez les termes et conditions.");
+      formData[6].setAttribute("data-error-visible", "true");
+      return false;
+    }else{
+      validate.style.display = "block";
+      setTimeout(3000);
+      return true;
+    }
   }
 }
