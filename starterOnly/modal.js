547040cb5956adb7d386bfa516a81977973f242e
Warrios974 <kenny.lauret@outlook.fr>
2022-08-31 21:24:12 +0400

    Modif Linter

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,12 +1,3 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // Initialisation des variable de vérification des champs
 let firstNameOk = false;
 let lastNameOk = false;
@@ -15,9 +6,9 @@ let birthdayOk = false;
 let participationOk = false;
 let cityTournamentOk = false;
 let termsOfUseOk = false;
+let regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+let regexNumber = /^([0-9]|[1-9][0-9]|100)$/;
 /////////////////////////////////////////////////////////
-
-
 // Initialisation des Elements du DOM nécessaire
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
@@ -32,46 +23,48 @@ const formReserve = document.querySelector("#reserve");
 const btnSubmit = document.querySelector(".btn-submit");
 const formDataInput = document.querySelectorAll(".formData > input");
 /////////////////////////////////////////////////////////
-
+function editNav() {
+  let x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 modalCloseBtn.addEventListener("click", function() {
-  closeModal(); 
-  displayFrom()
+  closeModal();
+  displayFrom();
 });
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 // Close modal form
 function closeModal() {
   modalbg.style.display = "none";
 }
-
 // Close modal form
 function displayFrom() {
   formReserve.style.display = "block";
   validateMessage.style.display = "none";
 }
-
-// fonctions qui fait apparaitre les erreurs et change les variable de validation////////////////////////////////////////////////////////////////
+//fonctions qui fait apparaitre les erreurs et change les variable de validation
 // Vérification de la longeur de l'input
-function twoCaractereOrmore(e,n){
-  e.length >= 2 ?(
+function twoCaractereOrmore(value,num){
+  value.length >= 2 ?(
     formData[n].removeAttribute("data-error-visible"),
-    n == 0 ? firstNameOk = true : lastNameOk = true
+    num == 0 ? firstNameOk = true : lastNameOk = true
   ) : (
     formData[n].setAttribute("data-error-visible", "true"),
-    n == 0 ? firstNameOk = false : lastNameOk = false
+    num == 0 ? firstNameOk = false : lastNameOk = false
   );
 }
-
 //Vérifie Si la valeur du champs ressemble a une adresse mail
 function emailValidate(e){
   //Utilisation des regex
-  /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(e) ? (
+  regexEmail.test(e) ? (
     formData[2].removeAttribute("data-error-visible"),
     emailOk = true
   ) : (
@@ -79,7 +72,6 @@ function emailValidate(e){
     emailOk = false
   );
 }
-
 //Vérifie Si la valeur du champs est vide
 function birthdayValidate(e){
   e ? (
@@ -90,12 +82,11 @@ function birthdayValidate(e){
     birthdayOk = false
   );
 }
-
 //Vérifie si c'est un nombre compris entre 1 et 100
 //Pour eviter que les gens ecrivent des chiffre abérent
 function participationValidate(e){
   //Utilisation des regex
-  /^([0-9]|[1-9][0-9]|100)$/.test(e) ? (
+  regexNumber.test(e) ? (
     formData[4].removeAttribute("data-error-visible"),
     participationOk = true
   ) : (
@@ -103,22 +94,18 @@ function participationValidate(e){
     participationOk = false
   );
 }
-
 //Vérifie si une ville est selectionné
-// Quand un des bouton radio et cliqué le valeur cityTournamentOk passe a vrais 
+// Quand un des bouton radio et cliqué le valeur cityTournamentOk passe a vrais
 radioCheckCity.forEach((input) => input.addEventListener("click", function(){
   cityTournamentOk = true;
 }));
-
 //cityTournamentOk est vrais
 function cityIsSelected(){
-  !cityTournamentOk ? 
-    formData[5].setAttribute("data-error-visible", "true") : 
+  !cityTournamentOk ?
+    formData[5].setAttribute("data-error-visible", "true") :
     formData[5].removeAttribute("data-error-visible");
 }
-
-
-//Quand la case des conditions est coché la valeur de termsOfUseOk change 
+//Quand la case des conditions est coché la valeur de termsOfUseOk change
 function termsOfUseIsChecked(){
   document.getElementById("checkbox1").checked ? (
       formData[6].removeAttribute("data-error-visible"),
@@ -128,17 +115,15 @@ function termsOfUseIsChecked(){
       termsOfUseOk = false
     );
 }
-/////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Submit
-// Quand le formulaire est "submit"
+//Submit
+//Quand le formulaire est "submit"
 formReserve.addEventListener("submit", function(e) {
   //l'évenement de base est annulé
   e.preventDefault();
   //Et la fonction validate est lancé
   validate();
 });
-// Function valider
+//Function valider
 function validate(){
   //Récupération des valeurs des champs
   let valueFirstName = formDataInput[0].value;
@@ -147,7 +132,6 @@ function validate(){
   let valueBirthday = formDataInput[3].value;
   let valueNbrParticipation = formDataInput[4].value;
   //////////////////////////////////////
-
   //Lancement des fonctions de vérification
   twoCaractereOrmore(valueFirstName,0);
   twoCaractereOrmore(valueLastName,1);
@@ -157,8 +141,7 @@ function validate(){
   cityIsSelected();
   termsOfUseIsChecked();
   //////////////////////////////////////////
-  
-  // Vérification des variables de validation
+  //Vérification des variables de validation
   if(firstNameOk && lastNameOk && emailOk && birthdayOk && participationOk && cityTournamentOk && termsOfUseOk){
     //Cache le formualaire et affiche le message de remerciement
     formReserve.style.display = "none";
